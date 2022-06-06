358263a52915c7b3c2b4867ad27e7853e8b52b6a
Lương Văn Tú <luongvantuit@gmail.com>
2022-06-06 22:38:19 +0700

    link firestore & implementation sdk

--- app/src/main/java/org/hamic/hv/ResultHandlerActivity.kt
+++ app/src/main/java/org/hamic/hv/ResultHandlerActivity.kt
@@ -2,10 +2,26 @@ package org.hamic.hv
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ResultHandlerActivity : AppCompatActivity() {
+
+    private val fireStore: FirebaseFirestore = FirebaseFirestore.getInstance()
+
+
+    private lateinit var id: String
+    private lateinit var blockHash: String
+    private lateinit var imgHash: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result_handler)
+        id = intent.getStringExtra("id")!!
+        blockHash = intent.getStringExtra("blockHash")!!
+        imgHash = intent.getStringExtra("imgHash")!!
+    }
+
+    fun verifyComplete() {
+
     }
 }
